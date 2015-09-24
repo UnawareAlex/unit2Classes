0f392624f946267d35309795cf6d370ff29c3969
UnawareAlex <agarnold@NN123-HA-22.HS.sd203.org>
2015-09-24 14:13:23 -0500

    Quick fix

--- Chapter 4 Practice/Chapter 4 practice/DistanceConverter.java
+++ Chapter 4 Practice/Chapter 4 practice/DistanceConverter.java
@@ -2,7 +2,7 @@ public class DistanceConverter
 {
    public static void main(String[] args)
    {
-      final double yards = 3.5;
+      double yards = 3.5;
       final double feet = yards * 3;
       final double inches = feet * 12;
       
