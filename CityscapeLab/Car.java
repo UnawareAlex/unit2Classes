83799c22438a6cba227adaa112575a95990eea9d
UnawareAlex <agarnold16@gmail.com>
2015-10-08 17:54:20 -0500

    10/8
    I have no clue what I’m doing at this point

--- CityscapeLab/Car.java
+++ CityscapeLab/Car.java
@@ -7,23 +7,19 @@ import java.awt.Color;
 import java.util.Random;
 
 /**
- * Write a description of class Buildings here.
+ * Write a description of class Car here.
  * 
  * @author Alex Arnold
  * @version 1
  */
-public class Building
+public class Car
 {
     /** description of instance variable x (add comment for each instance variable) */
     private int xLeft;
     private int yTop;
     private int width;
     private int height;
-    private int wndLeft;
-    private int wndTop;
-    private final int wndWidth;
-    private final int wndHeight;
-
+    
     /**
      * Constructs building with a given top left corner and predetermined length and heighth
      * @param   x   the x-cord of the top-left corner   
@@ -31,18 +27,14 @@ public class Building
      * @param   w   the width of the building (along the x axis)
      * @param   h   the height of the buuilding (along the y axis)
      */
-    public Building(int x, int y, int w, int h)
+    public Car(int x, int y, int w, int h)
     {
         // initialise instance variables
         xLeft = x;
         yTop = y;
         width = w;
         height = h;
-        wndLeft = x + 4;
-        wndTop = y + 4;
-        wndWidth = 2;
-        wndHeight = 2;
-        
+       
     }
     
     /**
@@ -52,16 +44,17 @@ public class Building
     public void draw(Graphics2D g2)
     {
         //main body of the building
-        Rectangle frame = new Rectangle(xLeft, yTop, width, height);
-        Rectangle window = new Rectangle(wndLeft, wndTop, wndWidth, wndHeight);
-
-        g2.setColor(Color.DARK_GRAY);
-        g2.draw(frame);
-        g2.fill(frame);
-        
-        g2.setColor(Color.YELLOW);
-        g2.draw(window);
-        g2.fill(window);
+        int move = 0;
+        int time = 0;
+        while (time < 5478392)
+        {
+            Rectangle car = new Rectangle(xLeft + move, yTop, width, height);
+            g2.setColor(Color.WHITE);
+            g2.draw(car);
+            g2.fill(car);
+            move += 10;
+            time +=1;
+        }
     }
 
 }
