47e00f3cdfd1f3c065ba7841d3023a8e28845257
UnawareAlex <agarnold16@gmail.com>
2015-11-18 20:09:31 -0600

    Time

--- CityscapeLab/CityscapeViewer.java
+++ CityscapeLab/CityscapeViewer.java
@@ -52,7 +52,7 @@ public class CityscapeViewer
         for( int seconds = 0; seconds < ANIMATION_TIME_IN_SECONDS; seconds++ )
         {
             component.nextFrame();
-            Thread.sleep( 1000 );
+            Thread.sleep( 250 );
         }
         
     }
